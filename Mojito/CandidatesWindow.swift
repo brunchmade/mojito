d78db447806871fd50da20c754b0cc4dc22f829d
Victor Lin <bornstub@gmail.com>
2015-12-30 20:19:45 -0800

    Fine tune UI

--- Mojito/CandidatesWindow.swift
+++ Mojito/CandidatesWindow.swift
@@ -10,7 +10,14 @@ import Cocoa
 import Foundation
 
 class CandidateWindow: NSWindow {
-    /*
+    private var initialLocation:NSPoint?
+    
+    override var canBecomeKeyWindow:Bool {
+        get {
+            return true
+        }
+    }
+    
     override init(contentRect: NSRect, styleMask aStyle: Int, backing bufferingType: NSBackingStoreType, `defer` flag: Bool) {
         super.init(contentRect: contentRect, styleMask: NSBorderlessWindowMask, backing: bufferingType, `defer`: flag)
         opaque = false
@@ -19,5 +26,31 @@ class CandidateWindow: NSWindow {
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-    }*/
+    }
+    
+    override func mouseDown(theEvent: NSEvent) {
+        self.initialLocation = theEvent.locationInWindow
+    }
+
+    /*
+    Once the user starts dragging the mouse, move the window with it. The window has no title bar for
+    the user to drag (so we have to implement dragging ourselves)
+    */
+    override func mouseDragged(theEvent: NSEvent) {
+        let screenVisibleFrame = NSScreen.mainScreen()!.visibleFrame
+        let windowFrame = frame
+        var newOrigin = windowFrame.origin
+        let currentLocation = theEvent.locationInWindow
+        newOrigin.x += currentLocation.x - initialLocation!.x
+        newOrigin.y += currentLocation.y - initialLocation!.y
+        
+        // Don't let window get dragged up under the menu bar
+        // Don't let window get dragged up under the menu bar
+        if ((newOrigin.y + windowFrame.size.height) > (screenVisibleFrame.origin.y + screenVisibleFrame.size.height)) {
+            newOrigin.y = screenVisibleFrame.origin.y + (screenVisibleFrame.size.height - windowFrame.size.height)
+        }
+        
+        // Move the window to the new location
+        setFrameOrigin(newOrigin)
+    }
 }
