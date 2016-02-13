1f2e8f5b40f304f5a4a3cec23c4b70cb5ad4eb6c
Victor Lin <bornstub@gmail.com>
2016-02-13 14:57:13 -0800

    Try to install dmgbuild before deployment

--- scripts/archive-build-dmg.sh
+++ scripts/archive-build-dmg.sh
@@ -6,5 +6,5 @@ export APP_VERSION=$(/usr/libexec/PlistBuddy -c "Print :ApplicationProperties:CF
 export APP_BUILD=$(/usr/libexec/PlistBuddy -c "Print :ApplicationProperties:CFBundleVersion" "$ARCHIVE_PATH/Info.plist")
 export ARCHIVE_PRODUCTS_PATH=$ARCHIVE_PATH/Products
 # You will need to install dmgbuild first, run "pip install dmgbuild"
-/Library/Frameworks/Python.framework/Versions/2.7/bin/dmgbuild -s Mojito/dmg_settings.py \
+dmgbuild -s Mojito/dmg_settings.py \
     "Mojito installation" build/mojito.dmg
